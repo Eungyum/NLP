7f2ec1ead6cc425e85e2446f2acaeb7ec08c979e
Engyum <139725432+Eungyum@users.noreply.github.com>
2023-11-19 01:06:17 +0900

    Created

---
+++ text_preprocessing_stopword_markup.py
@@ -0,0 +1,10 @@
+import numpy as np
+import re
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
